086c0f467b961de8cd506ce68b53b52221b0fc1f
Gabriela M. Fernández <galalou2010@gmail.com>
2023-03-19 22:44:00 +0100

    failed logic genrePage

--- src/pages/genre/GenrePage.jsx
+++ src/pages/genre/GenrePage.jsx
@@ -1,15 +1,39 @@
 import React from 'react';
-import genreImg from '../../assets/img/genre.svg'
-import SearchBar from '../../components/atoms/searchCollection/SearchCollection';
+import { useState, useEffect } from 'react';
+import genreImg from '../../assets/img/genre.svg';
+import SearchCategory from '../../components/atoms/searchCategory/SearchCategory';
+import SmallCard from '../../components/molecules/smallCard/SmallCard'
 
 function GenrePage() {
-  return (
+  const [searchResult, setSearchResults] = useState([]);
+  const [searchGenres, setSearchGenres] = useState("");
+  const apiUrl = `http://127.0.0.1:8000/api/quotes`
+
+  const handleSearch = (genreId) => {
+    fetch(apiUrl).then((response) => response.json()).then((data)=>{
+        const filteredQuotes = data.filter((quote) => quote.genre_ID === genreId);
+        setSearchResults(filteredQuotes);
+    })
+    .catch((error) =>{console.error('Error fetching quotes', error);
+    });
+    };
+  
+    return (
     <div className='flex'>
         <div className="flex flex-col gap-4 align-center">
-            
+            <SearchCategory text={"Type a genre ID number"} onSearch={handleSearch} />
             <img className='w-1/2' src={genreImg} alt="Genre Graphic" />
         </div>
-        <div className=''></div>
+        <div className='flex flex-col gap-5'>
+        {searchResult.map((quote) => (
+                   <SmallCard
+                   key={quote.id}
+                   title = {quote.title}
+                   text = {quote.text}
+               />
+      ))}
+ 
+        </div>
     </div>
   )
 }
