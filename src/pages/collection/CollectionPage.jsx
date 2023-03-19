8e4c568c4f6658427d9ed1323a92b3eebc685db8
Gabriela M. Fernández <galalou2010@gmail.com>
2023-03-19 20:25:16 +0100

    table with backend data

--- src/pages/collection/CollectionPage.jsx
+++ src/pages/collection/CollectionPage.jsx
@@ -5,34 +5,33 @@ import SearchBar from '../../components/atoms/searchBar/searchBar';
 import { Table } from 'flowbite-react';
 import { TableRow } from 'flowbite-react/lib/esm/components/Table/TableRow';
 import { TableBody } from 'flowbite-react/lib/esm/components/Table/TableBody';
+import '../collection/collectionPage.css';
 
 function CollectionPage() {
-    const [ quotes, setQuotes ] = useState([]);
+    
+    const [ quotes, setQuotes ] = useState();
     const apiURL = 'http://127.0.0.1:8000/api/quotes';
-    console.log(apiURL)
 
     useEffect(() => {
         const fetchData = async () => {
-
-            const response = await fetch(apiURL);
-            const data = await response.json();
-
-            //use only 5 quotes
-            setQuotes (data.slice(0, 5))
-        }
+            const response = await fetch(apiURL).then(res => res.json());
+            console.log(response)
+            setQuotes(response)
+        } 
         fetchData();
+        console.log({quotes});
     }, []);
 
   return (
     <div className='flex flex-col gap-5'>
-        <div className='flex justify-between'>
+        <div className='flex justify-between overflow-hidden'>
             <h2 style={{fontSize: '2rem'}}>Your quotes</h2>
             <div className='flex justify-end gap-5'>
                 <GeneralButton buttonType={'primary'} text={'Add quote'} />
                 <SearchBar />
             </div>
         </div>
-        <Table>
+            <Table >
                 <Table.Head>
                     <Table.HeadCell>
                         Quote
@@ -52,19 +51,20 @@ function CollectionPage() {
                 </Table.Head>
                 <TableBody>
                 {
-                    quotes.map( (quote,key) =>
-                        <TableRow key={key}>
+                    quotes && quotes.map(quote =>
+                        <TableRow key={quote.id}>
                             <Table.Cell>{quote.text}</Table.Cell>
-                            <Table.Cell>{quote.author}</Table.Cell>
-                            <Table.Cell>{quote.book ? quote.book.name : ""}</Table.Cell>
-                            <Table.Cell>{quote.genre ? quote.genre.name : ""}</Table.Cell>
+                            <Table.Cell>{quote.author?.name}</Table.Cell>
+                            <Table.Cell>{quote.book?.name}</Table.Cell>
+                            <Table.Cell>{quote.genre?.name}</Table.Cell>
                             <Table.Cell>{quote.updated_at}</Table.Cell>
                         </TableRow>
                     )
                 }
                 </TableBody>
-        </Table>
+            </Table>
     </div>
+
   )
 }
 
