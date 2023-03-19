d966a60426807dfd3d46f7f74bc7591be3c2bc8c
Gabriela M. Fernández <galalou2010@gmail.com>
2023-03-19 12:05:24 +0100

    change distribution slogan

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -8,8 +8,8 @@ function HomePage() {
     <div className='flex flex-col p-10 lg:flex-row gap-5'>
       <div className='flex flex-col justify-center  text-left text-2xl text-navy'>
         <h2 className='mb-10'>Don't trust your mind. <br />
-          Store quotes: they are <br />
-          lighter than books.</h2>
+          Store quotes:<br /> 
+          they are lighter than books.</h2>
           <Button style={{backgroundColor:'#ffc482', color:"black", fontWeight:"bold", fontFamily:'Average Sans', fontSize: 20 }}> Start quoting</Button>
       </div>
       <div className='flex justify-center align-middle'>
