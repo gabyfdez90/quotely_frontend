a384761e42bb9cd376291d816a0432d04cf7187a
Gabriela M. Fernández <galalou2010@gmail.com>
2023-03-20 01:10:47 +0100

    links in navbar

--- src/components/molecules/navBar/Navbar.jsx
+++ src/components/molecules/navBar/Navbar.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import React from 'react';
 import logo from "./../../../assets/img/quotelyLogo.png";
 import { HiOutlineMenuAlt1 } from 'react-icons/hi';
+import { Link } from 'react-router-dom';
 
 function NavBar() {
   const [showDrawer, setShowDrawer] = useState(false);
@@ -21,15 +22,21 @@ function NavBar() {
         <div className="flex flex-col">
         <div className={`fixed pt-2 top-40 left-0 z-20 w-64 h-full transition-all duration-500 transform ${showDrawer ? 'translate-x-0' : '-translate-x-full'} bg-maingreen shadow-lg`}>
           <div className="px-6 py-4">
+          <Link to="/add">
             <div className="text-lg font-semibold font-sans p-7 mb-10 bg-mustard" typeof='button'>
               <p>Add quote</p>
               </div>
+          </Link>
+          <Link to="/collection">
             <div className= "p-4 mt-4 font-sans bg-mustard flex-center align-center" typeof='button'>
               <p>Collection</p>
             </div>
+            </Link>
+            <Link to="/genre">
             <div className= "p-4 mt-4 font-sans bg-mustard flex-center align-center" typeof='button'>
               <p>Genre</p>
             </div>
+            </Link>
             <div className= "p-4 mt-4 font-sans bg-mustard flex-center align-center" typeof='button'>
               <p>Author</p>
             </div>
